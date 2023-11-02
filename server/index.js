8b6c16080b031e262dd4e2ca5b87d6103416d37d
Harsh <98403303+SoniHarsh1@users.noreply.github.com>
2023-11-02 14:44:59 +0530

    updates cors

--- server/index.js
+++ server/index.js
@@ -22,22 +22,13 @@ app.use("/images", express.static("images"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_LINK,
-//     methods: ['GET', 'POST', 'UPDATE', 'PUT'],
-//     credentials: true,
-//   }),
-// )
-
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions))
+app.use(
+  cors({
+    origin: process.env.CLIENT_LINK,
+    methods: ['GET', 'POST', 'UPDATE', 'PUT'],
+    credentials: true,
+  }),
+)
 
 app.get('/', (req, res) => {
   res.send('Hello from server')

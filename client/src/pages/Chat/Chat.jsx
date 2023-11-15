b20d788dd9b09c0cee698245b2e55f8bc0f0094f
Harsh <98403303+SoniHarsh1@users.noreply.github.com>
2023-11-16 04:41:34 +0530

    completes ui of chat section

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,53 +1,75 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { userChats } from '../../api/ChatRequest'
-import LogoSearch from '../../components/LogoSearch/LogoSearch'
-import Conversation from '../../components/Conversation/Conversation'
-import './Chat.css'
-
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { userChats } from "../../api/ChatRequest";
+import LogoSearch from "../../components/LogoSearch/LogoSearch";
+import Conversation from "../../components/Conversation/Conversation";
+import ChatBox from "../../components/ChatBox/ChatBox";
+import "./Chat.css";
+import { Link } from "react-router-dom";
+import Home from "../../img/home.png";
+import Noti from "../../img/noti.png";
+import Message from "../../img/comment.png";
+import { UilSetting } from "@iconscout/react-unicons";
 
 const Chat = () => {
   const { user } = useSelector((state) => state.authReducer.authData);
-  
-  const[chats, setChats]  = useState([]);
+
+  const [chats, setChats] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
 
   useEffect(() => {
-        const getChats = async () => {
-          try{
-            const {data} = await userChats(user._id);
-            setChats(data);
-            console.log(data);
-          } catch(error){
-              console.log(error);
-          }
-        }
-        getChats();
+    const getChats = async () => {
+      try {
+        const { data } = await userChats(user._id);
+        setChats(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getChats();
   }, [user]);
 
   return (
     <div className="Chat">
       {/* Left Side */}
       <div className="Left-side-chat">
-      <LogoSearch />
-      <div className="Chat-container">
-        <h2>Chats</h2>
-        <div className="Chat-list">
-          {chats.map((chat) => (
-            <div>
-              <Conversation data = {chat} currentUserId = {user._id} />
-            </div>
-          ))}
-        </div>
+        <LogoSearch />
+        <div className="Chat-container">
+          <h2>Chats</h2>
+          <div className="Chat-list">
+            {chats.map((chat) => (
+              <div
+                onClick={() => {
+                  setCurrentChat(chat);
+                }}
+              >
+                <Conversation data={chat} currentUserId={user._id} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
       {/* Right Side */}
       <div className="Right-side-chat">
-        Right side Chat
+        <div style={{ width: "20rem", alignSelf: "flex-end" }}>
+          <div className="navIcons">
+            <Link to="../home">
+              <img src={Home} alt="" />
+            </Link>
+            <UilSetting />
+            <img src={Noti} alt="" />
+            <Link to="../chat">
+              <img src={Message} alt="" />
+            </Link>
+          </div>
         </div>
+        {/* Chat Body */}
+        <ChatBox chat={currentChat} currentUserId={user._id} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
